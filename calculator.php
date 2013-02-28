1cad951da6c39a017fb9fadea23b9163079b5df8
ravindrakaranam <ravi@cloudmunch.com>
2013-02-28 16:10:17 +0530

    Sample php application

---
+++ calculator.php
@@ -0,0 +1,10 @@
+<?php
+
+echo "Calculator Application";
+
+?>
+
+<br>
+
+The sume of two numbers <?php echo $_GET["num1"]; ?> and <?php echo $_GET["num2"]; ?> is <br>
+<?php echo $_GET["num1"]+$_GET["num2"]; ?> !
